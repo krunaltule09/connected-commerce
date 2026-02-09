7ee5ffbfabaae156de9ede8c5a554db68cbe2ee8
krunaltule09 <krunaltule53@gmail.com>
2026-02-09 19:46:47 +0530

    fixed

--- src/pages/data-simulator/DataSimulator.jsx
+++ src/pages/data-simulator/DataSimulator.jsx
@@ -145,6 +145,7 @@ const DataSimulator = () => {
                     <Box className={styles.timelineIcon}>
                       <Box className={styles.sparkIcon}></Box>
                     </Box>
+                    <Box className={styles.timelineConnector}></Box>
                   </Box>
                   <Box className={styles.timelineContent}>
                     <Typography className={styles.timelineValue} sx={{ fontSize: '50px !important', mb: "10px !important"  }}>30 Minutes</Typography>
